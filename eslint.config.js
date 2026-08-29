03daa595d53dafd99bd0ce7b4499347e222ecfdb
dayannacastillo21 <castillovelasquezdayanna5@gmail.com>
2026-08-28 21:25:12 -0500

    chore(config): configurar entorno vite, tailwind css, eslint y variables de entorno

--- eslint.config.js
+++ eslint.config.js
@@ -17,5 +17,15 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'no-unused-vars': ['error', { caughtErrors: 'none' }],
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+  {
+    files: ['src/context/**/*.{js,jsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
   },
 ])
